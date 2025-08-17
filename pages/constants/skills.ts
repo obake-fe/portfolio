54efadc68cc8010e681e568ba00b95e5868b5c87
obake-fe <daichismash@gmail.com>
2025-08-17 17:45:30 +0900

    feat: skillページをお試し実装

---
+++ pages/constants/skills.ts
@@ -0,0 +1,83 @@
+export const skills = [
+  {
+    name: 'React',
+    icon: 'react.png',
+    years: 5,
+    comment: '主にフロント開発で使用'
+  },
+  {
+    name: 'Next.js',
+    icon: 'next.png',
+    years: 1.5,
+    comment: 'SSG/SSR対応のサイト構築'
+  },
+  {
+    name: 'TypeScript',
+    icon: 'typescript.png',
+    years: 5,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'GraphQL',
+    icon: 'graphql.png',
+    years: 2,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'Redux',
+    icon: 'redux.png',
+    years: 3,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'Storybook',
+    icon: 'storybook.png',
+    years: 2,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'Material UI',
+    icon: 'graphql.png',
+    years: 2,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'Chakra UI',
+    icon: 'graphql.png',
+    years: 2,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'tailwindcss',
+    icon: 'graphql.png',
+    years: 2,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'React Hook Form',
+    icon: 'graphql.png',
+    years: 2,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'Formik',
+    icon: 'graphql.png',
+    years: 2,
+    comment: '型安全な開発に必須'
+  },
+  {
+    name: 'Zod',
+    icon: 'graphql.png',
+    years: 2,
+    comment: '型安全な開発に必須'
+  }
+]
+
+export const testAndCISkills = [
+  {
+    name: 'Jest',
+    icon: 'jest.png',
+    years: 5,
+    comment: '実務経験あり'
+  }
+]
