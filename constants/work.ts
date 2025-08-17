d00962cb7570160b2f7463948900cbda24ff96fa
obake-fe <daichismash@gmail.com>
2025-08-17 21:42:49 +0900

    feat: workページの追加

---
+++ constants/work.ts
@@ -0,0 +1,63 @@
+export const career = [
+  {
+    year: '2017',
+    month: '10',
+    title: 'Frontend Engineer としてのキャリアを開始',
+    description: '静的Webページ制作'
+  },
+  {
+    year: '2019',
+    month: '4',
+    title: '就活支援Webアプリ運用、保守',
+    description: 'HTML / SCSS / jQuery'
+  },  {
+    year: '2020',
+    month: '10',
+    title: '障害福祉支援施設用業務効率化webアプリ開発',
+    description: 'React / Redux / TypeScript'
+  },  {
+    year: '2023',
+    month: '4',
+    title: 'サロン予約・顧客管理サービスSP版リプレイス案件',
+    description: 'Next.js / TypeScript / GraphQL'
+  },
+  {
+    year: '2024',
+    month: '7',
+    title: '新規ゲーム開発',
+    description: 'React / TypeScript'
+  }
+]
+export const work = [
+  {
+    title: 'Portfolio Site',
+    description: 'このサイトのこと。記事コンテスト用に作成',
+    link: 'https://portfolio-taupe-eight-56.vercel.app/',
+    tech: ['React', 'Next.js', 'TypeScript', 'Chakra UI']
+  },
+  {
+    title: '個人ブログ',
+    description: 'ちょっとしたアウトプットを行う場所として作成。Gatsby.js、GraphQLを使用し、記事のタグ付けや検索機能、OGP画像の自動作成機能などを追加した',
+    link: 'https://smashawk.com/',
+    tech: ['Gatsby.js', 'GraphQL']
+  },
+  {
+    title: 'React、TypeScript学習（外部API使用アプリ）',
+    description: 'React.js、TypeScriptの他にRedux（状態管理）、Jest（単体テスト）、Cypress（E2Eテスト）などを使用した',
+    link: 'https://laughing-cray-9d5255.netlify.app/',
+    tech: ['React', 'TypeScript', 'Redux', 'Jest', 'Cypress']
+  },
+  {
+    title: 'PHP学習（掲示板アプリ）',
+    description: 'PHP及びLaravelの学習の一環として作成。PHPUnitを使用した単体テストまで実施',
+    link: 'https://github.com/obake-fe/bulletin-board',
+    tech: ['PHP', 'Laravel', 'PHPUnit']
+  },
+  {
+    title: 'GraphQL学習（画像投稿アプリ）',
+    description: 'GraphQLの学習の一環として作成。Apollo Serverでサーバ実装、GitHub OAuthを利用したログイン機能などを実装',
+    link: 'https://github.com/obake-fe/learing-graphql',
+    tech: ['GraphQL', 'Apollo Server', 'GitHub OAuth']
+  }
+]
+
