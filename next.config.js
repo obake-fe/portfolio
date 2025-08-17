f83bc219698655fba650785bbab859a05e331065
obake-fe <daichismash@gmail.com>
2025-08-17 22:07:37 +0900

    feat: build時キャッシュクリアする機能を追加

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.tsx')
-module.exports = withNextra()
+module.exports = withNextra(
+  {
+    // Next.jsの設定
+    outputFileTracing: false,
+    // 必要であれば他の設定も追加可能
+  }
+)
